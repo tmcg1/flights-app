2dba360cb8e4e4a659e39c4c4f9e3d931ee31aa2
mihkeltina <mihkeltina@gmail.com>
2025-03-14 11:35:53 +0200

    Added more filters

--- src/views/Flights/Flights.js
+++ src/views/Flights/Flights.js
@@ -5,25 +5,62 @@ import { useNavigate } from "react-router-dom";
 function Flights() {
     const [flights, setFlights] = useState([]);
     const navigate = useNavigate();
+    const [fromFilter, setFromFilter] = useState("")
+    const [toFilter, setToFilter] = useState("")
+    const [departureFilter, setDepartureFilter] = useState("")
+    const [priceFilter, setPriceFilter] = useState("")
 
     function handleClick() {
         navigate("/seats");
     }
 
     useEffect(() => {
-        fetch('http://localhost:8080/flights')
+        fetch(`http://localhost:8080/flights?from=${fromFilter}&to=${toFilter}&departure=${departureFilter}&price=${priceFilter}`)
             .then(response => response.json())
             .then(data => setFlights(data));
-    }, []);
+    }, [fromFilter, toFilter, departureFilter, priceFilter]);
+
+    function handleFromFilter(e) {
+        setFromFilter(e.target.value)
+    }
+
+    function handleToFilter(e) {
+        setToFilter(e.target.value)
+    }
+
+    function handleDepartureFilter(e) {
+        setDepartureFilter(e.target.value)
+    }
+
+    function handlePriceFilter(e) {
+        setPriceFilter(e.target.value)
+    }
 
     return (
         <>
             <h1 className="title">Flights</h1>
+            <div>
+                <label>From: </label>
+                <input onChange={handleFromFilter}></input>
+            </div>
+            <div>
+                <label>To: </label>
+                <input onChange={handleToFilter}></input>
+            </div>
+            <div>
+                <label>Departure time: </label>
+                <input onChange={handleDepartureFilter}></input>
+            </div>
+            <div>
+                <label>Price: </label>
+                <input onChange={handlePriceFilter}></input>
+            </div>
             {flights.map(flight =>
                 <div className="card" key={flight.id} onClick={handleClick}>
                     <div>{flight.from}</div>
                     <div>{flight.to}</div>
                     <div>{flight.departure}</div>
+                    <div>{flight.price}€</div>
                 </div>
             )}
         </>
--- src/views/Seats/Seats.js
+++ src/views/Seats/Seats.js
@@ -3,16 +3,41 @@ import './Seats.css';
 
 function Seats() {
     const [seats, setSeats] = useState([]);
+    const [suggestWindowSeat, setSuggestWindowSeat] = useState(false);
+    const [numberOfSeats, setNumberOfSeats] = useState(1)
 
     useEffect(() => {
-        fetch('http://localhost:8080/seats')
+        fetch(`http://localhost:8080/seats?suggestWindowSeat=${suggestWindowSeat}&numberOfSeats=${numberOfSeats}`)
             .then(response => response.json())
             .then(data => setSeats(data));
-    }, []);
+    }, [suggestWindowSeat, numberOfSeats]);
+
+    function handleCheckboxClick() {
+        setSuggestWindowSeat(!suggestWindowSeat);
+    }
+
+    function increaseSeatNumber() {
+        setNumberOfSeats(numberOfSeats + 1)
+    }
+
+    function decreaseSeatNumber() {
+        if (numberOfSeats > 1)
+            setNumberOfSeats(numberOfSeats - 1)
+    }
 
     return (
         <>
             <h1>Seats</h1>
+            <div>
+                <input type="checkbox" onClick={handleCheckboxClick} />
+                <label> Window seat</label>
+            </div>
+            <div className="number-of-seats">
+                <label>Number of seats</label>
+                <button onClick={decreaseSeatNumber}>-</button>
+                {numberOfSeats}
+                <button onClick={increaseSeatNumber}>+</button>
+            </div>
             <div className="seats">
                 {seats.map(seating =>
                     <div className={`seating ${seating.isTaken ? 'is-taken' : ''} ${seating.isSuggested ? 'is-suggested' : ''}`} key={seating.id}>
